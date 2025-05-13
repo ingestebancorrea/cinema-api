3fe2d29ae5a71d6f58073983a18b1ae05fb6790c
Esteban Correa <estebancorrea405@gmail.com>
2025-05-13 11:14:42 -0500

    cambiando forma de pasar instancia de ticketUseCase a controller

--- src/infrastructure/controllers/ticket.controller.js
+++ src/infrastructure/controllers/ticket.controller.js
@@ -1,12 +1,4 @@
-const TicketUseCase = require('../../application/use-cases/ticket.usecase');
-const TicketRepositoryImpl = require('../repositories/ticket.repository.impl');
-const FunctionRepositoryImpl = require('../repositories/function.repository.impl');
-
-const ticketRepository = new TicketRepositoryImpl();
-const functionRepository = new FunctionRepositoryImpl();
-const ticketUseCase = new TicketUseCase(ticketRepository, functionRepository);
-
-const reserveTicket = (ticketUseCaseInstance = ticketUseCase) => async (req, res) => {
+const reserveTicket = (ticketUseCaseInstance) => async (req, res) => {
     try {
         const { functionId, buyer } = req.body;
 
--- src/infrastructure/routes/ticket.routes.js
+++ src/infrastructure/routes/ticket.routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { reserveTicket } = require('../controllers/ticket.controller');
+const TicketUseCase = require('../../application/use-cases/ticket.usecase');
+const TicketRepositoryImpl = require('../repositories/ticket.repository.impl');
+const FunctionRepositoryImpl = require('../repositories/function.repository.impl');
 
 const router = express.Router();
 
-router.post('/reserve', reserveTicket);
+const ticketRepository = new TicketRepositoryImpl();
+const functionRepository = new FunctionRepositoryImpl();
+const ticketUseCase = new TicketUseCase(ticketRepository, functionRepository);
+
+router.post('/reserve', reserveTicket(ticketUseCase));
 
 module.exports = router;
